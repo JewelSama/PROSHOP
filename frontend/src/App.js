84a5b78eb5dab472ca343afce91dbeb1323f103e
Jewel Osafile <osafilejewel@gmail.com>
2022-07-19 16:27:04 +0100

    header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import {Container} from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
-     Welcome to proshop
+
+     <Header /> 
+     <main className='py-3'>
+      <Container>
+      <h1>Welcome to proshop</h1>
+      </Container>
+     </main>
+     <Footer />
     </>
   );
 }
