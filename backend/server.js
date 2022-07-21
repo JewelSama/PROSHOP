a341b080e0571004770aacca718fe55e8836a1ef
Jewel Osafile <osafilejewel@gmail.com>
2022-07-21 13:12:55 +0100

    product route

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const express = require('express')
-const products = require('./data/products')
 const dotenv = require('dotenv')
 const connectDB = require('./config/db')
+const productRoutes = require('./routes/product.routes')
+
 
 dotenv.config()
 
@@ -13,13 +14,9 @@ app.get('/', (req, res) => {
     res.send('Api is running...')
 })
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
-})
+app.use('/api/products', productRoutes)
+
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENv} mode on port ${PORT}...`))
