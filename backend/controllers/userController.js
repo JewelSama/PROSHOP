b56098cc24381e575f63c3b597617c6a47143d4d
Jewel Osafile <osafilejewel@gmail.com>
2022-07-22 22:04:11 +0100

    register new user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -31,6 +31,43 @@ const authUser = asyncHandler(async(req, res) => {
 })
 
 
+//register new user
+
+const registerUser = asyncHandler(async(req, res) => {
+    
+    const {name,email,password} = req.body
+
+    const userExists = await User.findOne({email})
+
+    if(userExists){
+        res.status(400)
+        throw new Error('User already exits')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+    
+    if(user){
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+   
+
+})
+
+
+
 //get user profile
 
 const getUserProfile = asyncHandler(async(req, res) => {
@@ -50,4 +87,4 @@ const getUserProfile = asyncHandler(async(req, res) => {
    }
 })
 
-module.exports = {authUser, getUserProfile};
+module.exports = {authUser, getUserProfile, registerUser};
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,10 +2,11 @@ const express = require('express')
 const {protect} = require('../middleware/authMiddleware')
 
 const router = express.Router()
-const {authUser, getUserProfile} = require('../controllers/userController')
+const {authUser, getUserProfile, registerUser} = require('../controllers/userController')
 // const  = require('../controllers/userController')
 
 
+router.route('/').post(registerUser)
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile)
 
