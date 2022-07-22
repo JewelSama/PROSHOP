bf4961f1c062acd40c0dff0fede68c73357709ce
Jewel Osafile <osafilejewel@gmail.com>
2022-07-22 21:15:09 +0100

    auth middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -34,8 +34,20 @@ const authUser = asyncHandler(async(req, res) => {
 //get user profile
 
 const getUserProfile = asyncHandler(async(req, res) => {
-   res.send('success')
+   const user = await User.findById(req.user._id)
 
+
+   if(user) {
+    res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin
+    })
+   } else {
+    res.status(404)
+    throw new Error('User not found')
+   }
 })
 
 module.exports = {authUser, getUserProfile};
