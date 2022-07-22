ba81cc18e11b12aa9b5497f682340d2296a10c6f
Jewel Osafile <osafilejewel@gmail.com>
2022-07-22 19:33:59 +0100

    product controller

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,24 +1,14 @@
 const express = require('express')
-const product = require('../models/productModel')
-const asyncHandler = require('express-async-handler')
-const Product = require('../models/productModel')
+
 const router = express.Router()
 
-router.get('/', asyncHandler(async(req, res) => {
-    const products = await Product.find({})
-    res.json(products)
-}))
+const  getProducts = require('../controllers/productController')
+const  getProductById = require('../controllers/productController')
 
-router.get('/:id', asyncHandler(async(req, res) => {
-    const product = await Product.findById(req.params.id)
 
-    if(product){
-        res.json(product)
-    } else {
-        res.status(404)
-        throw new Error('Product not found')
-    }
-}))
+router.route('/').get(getProducts)
+
+router.route('/:id').get(getProductById)
 
 
 
