e17ffe790f9971cadf8adcc5bd4cf921f34251c1
Jewel Osafile <osafilejewel@gmail.com>
2022-07-23 21:06:14 +0100

    update Profile

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -2,13 +2,13 @@ const express = require('express')
 const {protect} = require('../middleware/authMiddleware')
 
 const router = express.Router()
-const {authUser, getUserProfile, registerUser} = require('../controllers/userController')
+const {authUser, getUserProfile, registerUser, updateUserProfile} = require('../controllers/userController')
 // const  = require('../controllers/userController')
 
 
 router.route('/').post(registerUser)
 router.post('/login', authUser)
-router.route('/profile').get(protect, getUserProfile)
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
 
 
 
