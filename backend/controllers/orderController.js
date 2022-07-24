af66ac0defe629f5d603e3f28b69f6d721bbef3a
Jewel Osafile <osafilejewel@gmail.com>
2022-07-24 16:26:55 +0100

    order Controller

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,39 @@
+const asyncHandler = require('express-async-handler')
+const Order = require('../models/orderModel')
+
+//Create new prder
+// @route POST /api/users/orders
+//@access Private
+
+
+const addOrderItems = asyncHandler(async(req, res) => {
+    const {
+        orderItems,
+        shippingAddress,
+        paymentMethod,
+        itemsPrice,
+        taxPrice,
+        shippingPrice,
+        totalPrice,
+    } = req.body
+
+    if(orderItems && orderItems.length === 0){
+        res.status(400)
+        throw new Error('No order items')
+        return 
+    } else {
+        const order = new Order({
+            orderItems,
+            user: req.user._id,
+            shippingAddress,
+            paymentMethod,
+            itemsPrice,
+            taxPrice,
+            shippingPrice,
+            totalPrice,
+        })
+        const createdUser = await order.save()
+
+        res.status(201).json(createdUser)
+    }
+})
