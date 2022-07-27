501aaeb8724d96d3e5d9831b5b33842e75174f76
Jewel Osafile <osafilejewel@gmail.com>
2022-07-27 20:03:38 +0100

    getUsersList constant, reducer && action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import {cartReducer} from './reducers/cartReducers'
-import {userDetailsReducer,userUpdateProfileReducer, userLoginReducer, userRegisterReducer} from './reducers/userReducers'
+import {userDetailsReducer,userUpdateProfileReducer, userLoginReducer, userRegisterReducer,} from './reducers/userReducers'
 import {orderCreateReducer, orderDetailsReducer, orderListMyReducer, orderPayReducer} from './reducers/orderReducers'
 
 
