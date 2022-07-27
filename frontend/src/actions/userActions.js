c02633bb6d4bf5b40c1608b43a23bbc09b4837bc
Jewel Osafile <osafilejewel@gmail.com>
2022-07-27 17:21:34 +0100

    userDetails and Orders Reset

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
-import {USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL, USER_LOGOUT, USER_REGISTER_REQUEST, USER_REGISTER_FAIL, USER_REGISTER_SUCCESS, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS, USER_DETAILS_FAIL, USER_UPDATE_PROFILE_REQUEST, USER_UPDATE_PROFILE_SUCCESS, USER_UPDATE_PROFILE_FAIL} from '../constants/userConstant'
+import {USER_LOGIN_REQUEST, USER_DETAILS_RESET, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL, USER_LOGOUT, USER_REGISTER_REQUEST, USER_REGISTER_FAIL, USER_REGISTER_SUCCESS, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS, USER_DETAILS_FAIL, USER_UPDATE_PROFILE_REQUEST, USER_UPDATE_PROFILE_SUCCESS, USER_UPDATE_PROFILE_FAIL} from '../constants/userConstant'
+import {ORDER_LIST_MY_RESET} from '../constants/orderConstants'
+
+
 
 export const login = (email, password) => async(dispatch) => {
     try {
@@ -40,6 +43,8 @@ export const login = (email, password) => async(dispatch) => {
 export const logout = () => (dispatch) => {
     localStorage.removeItem('userInfo')
     dispatch({type: USER_LOGOUT})
+    dispatch({type: USER_DETAILS_RESET})
+    dispatch({type: ORDER_LIST_MY_RESET})
 }
 
 
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,5 +1,5 @@
-import {USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL, USER_LOGOUT, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS, USER_DETAILS_FAIL, USER_UPDATE_PROFILE_REQUEST, USER_UPDATE_PROFILE_SUCCESS, USER_UPDATE_PROFILE_FAIL} from '../constants/userConstant'
-import {USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_REGISTER_FAIL, } from '../constants/userConstant'
+import {USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL, USER_LOGOUT, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS, USER_DETAILS_FAIL, USER_UPDATE_PROFILE_REQUEST, USER_UPDATE_PROFILE_SUCCESS, USER_UPDATE_PROFILE_FAIL, USER_DETAILS_RESET} from '../constants/userConstant'
+import {USER_REGISTER_REQUEST,  USER_REGISTER_SUCCESS, USER_REGISTER_FAIL, } from '../constants/userConstant'
 
 
 export const userLoginReducer = (state = {}, action) => {
@@ -50,7 +50,8 @@ export const userDetailsReducer = (state = { user: {} }, action) => {
         case USER_DETAILS_FAIL:
             return {loading: false, error: action.payload}
          
-       
+        case USER_DETAILS_RESET: 
+            return{user: {}}
             default:
                 return state
     }
