9ff597368f6001a849cb6fa618b33103c2531626
Jewel Osafile <osafilejewel@gmail.com>
2022-07-28 19:47:42 +0100

    deleteProduct on frontend

--- frontend/src/screens/ProductListScreen.jsx
+++ frontend/src/screens/ProductListScreen.jsx
@@ -5,7 +5,7 @@ import {LinkContainer} from 'react-router-bootstrap'
 import {useDispatch, useSelector} from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import {listProducts} from '../actions/productActions'
+import {listProducts, deleteProduct} from '../actions/productActions'
 
 
 
@@ -15,6 +15,9 @@ const ProductListScreen = () => {
 
     const productList = useSelector(state => state.productList)
     const {loading, error, products} = productList
+    
+    const productDelete = useSelector(state => state.productDelete)
+    const {loading:loadingDelete, error:errorDelete, success:successDelete} = productDelete
     
     const userLogin = useSelector(state => state.userLogin)
     const {userInfo} = userLogin
@@ -26,12 +29,12 @@ const ProductListScreen = () => {
         } else {
             navigate('/')
         }
-    }, [dispatch, navigate, userInfo])
+    }, [dispatch, navigate, userInfo, successDelete])
 
     const deleteHandler = (id) => {
         if(window.confirm('Are you sure?'))
         {
-            //Delete Products
+            dispatch(deleteProduct(id))
         }
     }
 
@@ -52,6 +55,8 @@ const ProductListScreen = () => {
             </Button>
         </Col>
     </Row>
+    {loadingDelete && <Loader />}
+    {errorDelete && <Message variant="danger">{errorDelete}</Message>}
         {loading ? <Loader /> : error ? <Message variant="danger">{error}</Message> : (
             <Table striped bordered hover responsive className='table-sm'>
                 <thead>
