e945d12ea69f87acb9736fa5585f0208892f4bf2
Jewel Osafile <osafilejewel@gmail.com>
2022-07-28 15:44:35 +0100

    userUpdate for admin..

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import {cartReducer} from './reducers/cartReducers'
-import {userDetailsReducer,userDeleteReducer, userUpdateProfileReducer, userLoginReducer, userRegisterReducer, userListReducer} from './reducers/userReducers'
+import {userDetailsReducer, userUpdateReducer ,userDeleteReducer, userUpdateProfileReducer, userLoginReducer, userRegisterReducer, userListReducer} from './reducers/userReducers'
 import {orderCreateReducer, orderDetailsReducer, orderListMyReducer, orderPayReducer} from './reducers/orderReducers'
 
 
@@ -16,6 +16,7 @@ const reducer = combineReducers({
     userRegister: userRegisterReducer,
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
+    userUpdate: userUpdateReducer,
     userList: userListReducer,
     userDelete: userDeleteReducer,
     orderCreate: orderCreateReducer,
