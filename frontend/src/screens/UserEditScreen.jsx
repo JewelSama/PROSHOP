75afede50bde02f9ee7ef4e9e11077d07e41eb8c
Jewel Osafile <osafilejewel@gmail.com>
2022-07-28 16:19:24 +0100

    rendered update User in frontend

--- frontend/src/screens/UserEditScreen.jsx
+++ frontend/src/screens/UserEditScreen.jsx
@@ -1,12 +1,13 @@
 import React, {useState, useEffect} from 'react'
-import {Link, useParams, useLocation, useNavigate} from 'react-router-dom'
+import {Link, useParams, useNavigate} from 'react-router-dom'
 import {Form, Button} from 'react-bootstrap'
 import {useDispatch, useSelector} from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import {getUserDetails} from '../actions/userActions'
+import {getUserDetails, updateUser} from '../actions/userActions'
 import FormContainer from '../components/FormContainer'
-import { useImperativeHandle } from 'react'
+import {USER_UPDATE_RESET} from '../constants/userConstant'
+
 
 const UserEditScreen = () => {
     const params = useParams()
@@ -23,23 +24,32 @@ const UserEditScreen = () => {
 
 
     const userDetails = useSelector(state => state.userDetails)
-
     const {loading, error, user } = userDetails
+
+    const userUpdate = useSelector(state => state.userUpdate)
+    const {loading:loadingUpdate, error:errorUpdate, success:successUpdate } = userUpdate
 
 
     useEffect(() => {
-        if(!user.name || user._id !== userId){
-            dispatch(getUserDetails(userId))
+        if(successUpdate){
+            dispatch({type: USER_UPDATE_RESET})
+            navigate('/admin/userlist')
         } else {
-            setName(user.name)
-            setEmail(user.email)
-            setIsAdmin(user.isAdmin)
+            if(!user.name || user._id !== userId){
+                dispatch(getUserDetails(userId))
+            } else {
+                setName(user.name)
+                setEmail(user.email)
+                setIsAdmin(user.isAdmin)
+            }
         }
 
-    }, [userId, dispatch, user])
+    }, [userId, dispatch, user, navigate, successUpdate])
 
     const submitHandler = (e) => {
         e.preventDefault();
+        dispatch(updateUser({_id: userId, name, email, isAdmin:isAdmin}))
+        console.log(isAdmin)
     }
 
   return (
@@ -50,6 +60,8 @@ const UserEditScreen = () => {
     
         <FormContainer>
         <h1>Edit User</h1>
+        {loadingUpdate && <Loader />}
+        {errorUpdate && <Message variant="danger">{errorUpdate}</Message>}
         {loading ? <Loader /> : error ? <Message variant="danger">{error}</Message> : (
         <Form onSubmit={submitHandler}>
         <Form.Group controlId='name'>
