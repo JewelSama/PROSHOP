24bd4aa86d084432067a17cb6cf35777f0dddc12
Jewel Osafile <osafilejewel@gmail.com>
2022-07-28 19:15:56 +0100

    delete Product for admin

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -21,6 +21,24 @@ const getProductById = asyncHandler(async(req, res) => {
         throw new Error('Product not found')
     }
 
+    
 })
 
-module.exports = {getProducts, getProductById}
+//delete product (admin)
+
+const deleteProduct = asyncHandler(async(req, res) => {
+    
+    const product = await Product.findById(req.params.id)
+
+    if(product){
+        await product.remove()
+        res.json({message: 'Product removed'})
+
+    } else {
+        res.status(404)
+        throw new Error('Product not found')
+    }
+
+})
+
+module.exports = {getProducts, getProductById, deleteProduct}
--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,4 +1,8 @@
+const {admin, protect} = require('../middleware/authMiddleware')
+const {deleteProduct} = require('../controllers/productController')
 const express = require('express')
+
+
 
 const router = express.Router()
 
@@ -7,7 +11,7 @@ const  {getProducts, getProductById} = require('../controllers/productController
 
 router.route('/').get(getProducts)
 
-router.route('/:id').get(getProductById)
+router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct)
 
 
 
