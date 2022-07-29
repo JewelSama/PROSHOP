b73961e7c1701642a91a1ac14d5173a60291e2ad
Jewel Osafile <osafilejewel@gmail.com>
2022-07-29 23:05:30 +0100

    submit review in frontend

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { productListReducer, productUpdateReducer, productCreateReducer, productDeleteReducer, productDetailsReducer } from './reducers/productReducers'
+import { productListReducer, productUpdateReducer, productReviewCreateReducer, productCreateReducer, productDeleteReducer, productDetailsReducer } from './reducers/productReducers'
 import {cartReducer} from './reducers/cartReducers'
 import {userDetailsReducer, userUpdateReducer ,userDeleteReducer, userUpdateProfileReducer, userLoginReducer, userRegisterReducer, userListReducer} from './reducers/userReducers'
 import {orderCreateReducer, orderDeliverReducer, orderDetailsReducer, orderListMyReducer, orderListReducer, orderPayReducer} from './reducers/orderReducers'
@@ -14,6 +14,7 @@ const reducer = combineReducers({
     productDelete: productDeleteReducer,
     productCreate : productCreateReducer,
     productUpdate: productUpdateReducer,
+    productReviewCreate: productReviewCreateReducer,
     cart: cartReducer,
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
