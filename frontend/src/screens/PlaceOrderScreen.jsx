88e956366539edb918b4a2920c8ab56b52e80571
Jewel Osafile <osafilejewel@gmail.com>
2022-07-29 11:45:17 +0100

    minor changes

--- frontend/src/screens/PlaceOrderScreen.jsx
+++ frontend/src/screens/PlaceOrderScreen.jsx
@@ -87,7 +87,7 @@ const placeOrderHandler = () => {
                                                 </Link> 
                                             </Col>
                                             <Col md={4}>
-                                                {item.qty} x {item.price} = $${(item.qty * item.price).toFixed(2)}
+                                                {item.qty} x {item.price} = ${(item.qty * item.price).toFixed(2)}
                                             </Col>
                                         </Row>
                                     </ListGroup.Item>
