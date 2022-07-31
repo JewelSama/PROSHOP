ae49f97f31f16af8ad04f344e4fe387e4f8d4f08
Jewel Osafile <osafilejewel@gmail.com>
2022-07-31 12:46:34 +0100

    fixed multer bugs

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -3,7 +3,7 @@ const Product = require('../models/productModel')
 
 
 const getProducts = asyncHandler(async(req, res) => {
-    const pageSize = 6
+    const pageSize = 4
     const page = Number(req.query.pageNumber) || 1
 
     const keyword = req.query.keyword
