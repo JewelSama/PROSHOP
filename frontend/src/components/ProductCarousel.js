29d71efb513a0f34e6e5f561dd52c760583a3d47
Jewel Osafile <osafilejewel@gmail.com>
2022-07-31 12:06:48 +0100

    fix bugs

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -22,9 +22,9 @@ const ProductCarousel = () => {
     return loading ? <Loader /> : error ? (<Message variant="danger">{error}</Message>) : (
         <Carousel pause='hover' className="bg-dark">
             {products.map((product) => (
-                <Carousel.Item  key={product._id}>
+                <Carousel.Item    key={product._id}>
                     <Link to={`/product/${product._id}`}>
-                        <Image src={product.image} alt={product.name} roundedCircle fluid />
+                        <Image style={{height: "300px"}} src={product.image} alt={product.name} fluid />
                         <Carousel.Caption className="carousel-caption">
                             <h2>{product.name} (${product.price})</h2>
                         </Carousel.Caption>
--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -7,6 +7,7 @@ import Message from '../components/Message'
 import Loader from '../components/Loader'
 import {getUserDetails, updateUserProfile} from '../actions/userActions'
 import {listMyOrders} from '../actions/orderActions'
+import {USER_UPDATE_PROFILE_RESET} from '../constants/userConstant'
 
 
 const ProfileScreen = () => {
@@ -40,7 +41,8 @@ const ProfileScreen = () => {
         if(!userInfo) {
             navigate('/login')
         } else {
-            if(!user.name){
+            if(!user || !user.name || success){
+                dispatch({type: USER_UPDATE_PROFILE_RESET})
                 dispatch(getUserDetails('profile'))
                 dispatch(listMyOrders())
             } else {
@@ -48,7 +50,7 @@ const ProfileScreen = () => {
                 setName(user.name)
             }
         }
-    }, [dispatch, navigate, userInfo, user])
+    }, [dispatch, navigate, userInfo, user, success])
 
     const submitHandler = (e) => {
         e.preventDefault();
